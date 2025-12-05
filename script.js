4f60df848e6c8819f33f3bf3e6b59cfd1da76e24
khiettt <khitam.bakkali.nebkhout@hva.nl>
2025-12-05 09:44:10 +0100

    commets toegevoegd

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Selecteer de button om de doelgroepen te zien
 let filterButton = document.querySelector('#beforeButton')
-
+// Wacht tot iemand erop klikt
 filterButton.addEventListener('click', function() {
+  // Selecteer een doelgroep 
   document.querySelector('.flag').classList.toggle('visible')
 });
